0eee1ceb9d58ab86508125e1db68aeccdb01ae03
ProfaTatianeTobias <93044628+ProfaTatianeTobias@users.noreply.github.com>
2022-05-04 21:33:28 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,6 +20,8 @@ var pernalonga;
 var coelho;
 var cortador;
 var salsicha, sadBoy, piscando;
+var somFundo, somAr, somComendo, somTriste, somCorte;
+var mudo;
 
 function preload (){
   parede = loadImage ("background.png");
@@ -28,6 +30,11 @@ function preload (){
   salsicha = loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
   sadBoy = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
   piscando = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  somFundo = loadSound("sound1.mp3");
+  somAr = loadSound("air.wav");
+  somComendo = loadSound("eating_sound.mp3");
+  somTriste = loadSound("sad.wav");
+  somCorte = loadSound("rope_cut.mp3");
 
   //playing e looping
   salsicha.playing = true;
@@ -43,6 +50,11 @@ function setup()
 {
   createCanvas(500,700);
   frameRate(80); //taxa de frames, geralmente: 30 frames/seg
+
+  //som de fundo
+  somFundo.play();
+  somFundo.setVolume(0.1);
+
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(200,680,600,20);
@@ -61,10 +73,16 @@ function setup()
 
   coelho.scale= 0.25; 
 
-cortador = createImg ("cut_btn.png");
-cortador.position (185,35);
-cortador.size (70,70);
-cortador.mouseClicked (cerveja);
+  //botão para mutar o som de fundo
+  mudo = createImg("mute.png");
+  mudo.position(400,50);
+  mudo.size(50,50);
+  mudo.mouseClicked(mutar);
+
+  cortador = createImg ("cut_btn.png");
+  cortador.position (185,35);
+  cortador.size (70,70);
+  cortador.mouseClicked (cerveja);
   var fruit_options = {
     density: 0.001
   }
@@ -89,11 +107,23 @@ function draw()
   ground.show();
   rope.show();
 
-  image(melancia,fruit.position.x,fruit.position.y,70,70);
-  
-  Engine.update(engine);
 
-  drawSprites ()
+  if (fruit!=null){
+
+  image(melancia,fruit.position.x,fruit.position.y,70,70);
+}  
+  Engine.update(engine);
+if (bateu(fruit,coelho)== true )
+{
+  coelho.changeAnimation ("comendo");
+
+
+}
+if(bateu(fruit,ground.body)== true) {
+  coelho.changeAnimation ("triste");
+}
+drawSprites ();
+
 }
  
  function cerveja (){
@@ -115,6 +145,15 @@ function bateu(body,sprite){
     }
   }
 
+}
+
+function mutar(){
+  if(somFundo.isPlaying()){
+    somFundo.stop();
+  }
+  else{
+    somFundo.play();
+  }
 }
 
 
